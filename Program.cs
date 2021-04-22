e24b496df1c2c968145ce76ac10a34da4b7df987
SantiFagliano <fagliano.santi@gmail.com>
2021-04-22 00:11:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos nombres desea ingresar?");
+            int cantidad = Int32.Parse(Console.ReadLine());
+            List<string> nombres = new List<string>();
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Ingrese un nombre: ");
+                string nombre = Console.ReadLine();
+                nombres.Add(nombre);
+            }
+            Console.WriteLine(" ");
+            nombres.Sort();
+            nombres.ForEach(Console.WriteLine);
+        }
+    }
+}
